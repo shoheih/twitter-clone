eaee7333ae72785db870c8f7b9505a73c9868580
shoheih <shoheigithub@gmail.com>
2019-08-13 11:29:45 +0900

    change primary theme color

--- src/theme.ts
+++ src/theme.ts
@@ -3,9 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   palette: {
     primary: {
-      light: '#63a4ff',
-      main: '#1976d2',
-      dark: '#004ba0',
+      light: '#4dabf5',
+      main: '#2196f3',
+      dark: '#1769aa',
       contrastText: '#fff'
     },
     secondary: {
