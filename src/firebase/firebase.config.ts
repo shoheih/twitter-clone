28641de8a315d8daa3e1de3c346de418dd691352
shoheih <shoheigithub@gmail.com>
2019-08-15 15:49:51 +0900

    delete config

--- src/firebase/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDWSMZIklqWqXdfBKCpCHoQL0tnsqxdhB0',
-  authDomain: 'twitter-clone-55291.firebaseapp.com',
-  databaseURL: 'https://twitter-clone-55291.firebaseio.com',
-  projectId: 'twitter-clone-55291',
-  storageBucket: '',
-  messagingSenderId: '248908990385',
-  appId: '1:248908990385:web:f210ce79ae98641d'
-};
