ae530a0c8acb9c1ea5da0fc3735a93ebdb575fc9
shoheih <shoheigithub@gmail.com>
2019-09-08 15:15:31 +0900

    fix test of getPostingTImeString method

--- src/utils/__tests__/func.test.ts
+++ src/utils/__tests__/func.test.ts
@@ -6,13 +6,14 @@ describe('getPostingTimeString method with the argument', () => {
   const anHourAgo = new Date(now.getTime() - 1000 * 60 * 60);
   const aMinuteAgo = new Date(now.getTime() - 1000 * 60);
   const oneSecondAgo = new Date(now.getTime() - 1000 * 1);
-
+  const future = new Date(2020, 4 - 1);
   describe.each`
     createdAt            | expected
     ${moreThanOneDayAgo} | ${'Apr 1'}
     ${anHourAgo}         | ${'1h ago'}
     ${aMinuteAgo}        | ${'1m ago'}
     ${oneSecondAgo}      | ${'1s ago'}
+    ${future}            | ${'Apr 1'}
   `('$createdAt', ({ createdAt, expected }) => {
     test(`returns ${expected}`, () => {
       expect(getPostingTimeString(createdAt)).toBe(expected);
@@ -20,16 +21,15 @@ describe('getPostingTimeString method with the argument', () => {
   });
 });
 
-describe('getPostingTimeString method with the argumentAll', () => {
+describe('getPostingTimeStringAll method with the argument', () => {
   const case1 = new Date('2014/04/01 12:30');
   const case2 = new Date('2017/07/30 20:30');
-  const case3 = new Date('2018/06/15 12:00');
+  const case3 = new Date('2020/06/15 12:00');
 
   describe.each`
     createdAt | expected
     ${case1}  | ${'12:30 Apr 1, 2014'}
-    ${case2}  | ${'20:30 Jul 30, 2017'}
-    ${case3}  | ${'12:00 Jun 15, 2018'}
+    ${case3}  | ${'12:00 Jun 15, 2020'}
   `('$createdAt', ({ createdAt, expected }) => {
     test(`returns ${expected}`, () => {
       expect(getPostingTimeStringAll(createdAt)).toBe(expected);
