6bc35a4197871bbb525d03c200a76df5ed5e078c
shoheih <shoheigithub@gmail.com>
2019-09-24 07:10:13 +0900

    modify ui

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -1,40 +1,10 @@
 import React from 'react';
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  useTheme,
-  useMediaQuery
-} from '@material-ui/core';
-import { CloseOutlined } from '@material-ui/icons';
+import { Dialog, DialogContent, DialogTitle } from '@material-ui/core';
 import { ModalProps } from './modal.types';
-import useStyles from './modal.styles';
 
 const Modal = ({ children, title, hide }: ModalProps) => {
-  const classes = useStyles();
-  const theme = useTheme();
-  const matchMediaQuery = useMediaQuery(theme.breakpoints.down('xs'));
-
   return (
-    <Dialog
-      fullScreen={matchMediaQuery}
-      fullWidth={true}
-      maxWidth={'sm'}
-      open={true}
-      onClose={hide}
-    >
-      {matchMediaQuery && (
-        <IconButton
-          className={classes.button}
-          edge="start"
-          color="inherit"
-          aria-label="close"
-          onClick={hide}
-        >
-          <CloseOutlined />
-        </IconButton>
-      )}
+    <Dialog fullWidth={true} maxWidth={'sm'} open={true} onClose={hide}>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>{children}</DialogContent>
     </Dialog>
